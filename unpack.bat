c578763851557a40ab2cc5584da7d295a6976e8d
AndroidUser97 <isaacnaylor305@gmail.com>
2021-06-21 19:59:32 -0600

    Removed files that were no longer needed

--- unpack.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-ECHO STUFF
-"C:\Program Files (x86)\Steam\steamapps\common\Starbound\win32\asset_unpacker.exe"
-"C:\Program Files (x86)\Steam\steamapps\common\Starbound\assets\packed.pak"
-"C:\Program Files (x86)\Steam\steamapps\common\Starbound\assets\unpacked"
-PAUSE
